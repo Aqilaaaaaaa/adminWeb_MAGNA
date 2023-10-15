8ce00ce5fc7f95fb0f445dd841f851c9ed40e3ea
HAQILA NUR NOUVADILA <haqilaan@gmail.com>
2023-10-15 23:02:51 +0700

    #1 Component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import SideNav from './components/SideNav';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <SideNav/>
+      <Footer/>
     </div>
   );
 }
